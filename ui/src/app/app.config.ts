423c6306509fd1228c5800e4d08d7763a245e593
Sandip Chitale <sandipchitale@gmail.com>
2024-11-22 22:02:22 -0800

    Make theming work. Compact table.

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -1,15 +1,22 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import {providePrimeNG} from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     providePrimeNG({
-      theme: Aura,
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: 'p',
+          darkModeSelector: '.dark-theme'
+        }
+      },
       ripple: true
     })]
 };
